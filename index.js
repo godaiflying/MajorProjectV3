166354930bb22ecf33c43f0288f32b6b6c2d86bd
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-06-14 14:09:50 +1000

    nothing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+//requests the html and css by 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
